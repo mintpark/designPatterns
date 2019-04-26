b7e4731468afd4e84a59dd12d96216e45b6af68c
mintpark <greatmint8@gmail.com>
2019-04-26 20:27:56 +0900

    remove test code

--- DesignPatterns.playground/Contents.swift
+++ DesignPatterns.playground/Contents.swift
@@ -3,16 +3,3 @@
 import UIKit
 import Swift
 import Foundation
-
-var list = [1,2,7,5,3,4,5]
-var valid = 3
-
-let aa = list.enumerated().first(where: { $0.element == valid })
-aa
-
-let bb = list.enumerated().first { (arg) -> Bool in
-    let (offset, element) = arg
-    return element == valid
-}
-bb
-
