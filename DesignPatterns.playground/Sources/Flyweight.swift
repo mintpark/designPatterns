6ed5b972b15e1695569a3770871a0e9a7e5f495a
Hayoung Park <greatmint8@gmail.com>
2019-07-21 22:53:47 +0900

    seperate Flyweight - 게임같은데선 잘 쓰이겠당 TIL - intrinsicContentSize는 cell 안에서는 의미가 없다. cell height는 table에서 정한다.

---
+++ DesignPatterns.playground/Sources/Flyweight.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+/*
+ 
+ Flyweight
+ 자원에 대한 비용이 클 때(중복 생성될 가능성 높거나, 비용은 큰데 사용빈도는 낮은) 비용을 아끼기 위해.
+ 
+ */
+
+struct SpecialityCoffee {
+    let origin: String
+}
+
+protocol CoffeeSearching {
+    func search(origin: String) -> SpecialityCoffee?
+}
+
+final class Menu: CoffeeSearching {
+    private var coffeeAvailable: [String: SpecialityCoffee] = [:]   // search 쓰려고 dict 쓴건가
+    
+    func search(origin: String) -> SpecialityCoffee? {  // 중복생성을 방지. 이미 생성된게 있으면 그걸 쓴다.
+        if coffeeAvailable.index(forKey: origin) == nil {
+            coffeeAvailable[origin] = SpecialityCoffee(origin: origin)  // 사용할 때 생성. 찾아보고 없으면 생성하니까.
+        }
+        
+        return coffeeAvailable[origin]
+    }
+}
+
+final class CoffeeShop {
+    private var orders: [Int: SpecialityCoffee] = [:]
+    private let menu: CoffeeSearching
+    
+    init(menu: CoffeeSearching) {
+        self.menu = menu
+    }
+    
+    func takeOrder(origin: String, table: Int) {
+        orders[table] = menu.search(origin: origin)
+    }
+    
+    func serve() {
+        for (table, origin) in orders {
+            print("serving \(origin) to table \(table)")
+        }
+    }
+}
+/*
+let coffeeShop = CoffeeShop(menu: Menu())
+
+coffeeShop.takeOrder(origin: "Yirgacheff, Ethiopia", table: 1)
+coffeeShop.takeOrder(origin: "Buziraguhindwa, Brundi", table: 2)
+coffeeShop.takeOrder(origin: "Yirgacheff, Ethiopia", table: 1)
+
+coffeeShop.serve()
+*/
